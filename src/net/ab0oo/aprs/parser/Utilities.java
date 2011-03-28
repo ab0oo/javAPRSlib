7ac545cbf1ae45d19c9ba8d7681e4e1644823cdb
ab0oo <ab0oo@arrl.net>
2011-03-28 12:05:40 +0000

    Synchronization update.  This is working Wedjat code, with a few tweaks to the parsers. Weather parsing is coming up next.  The WEB code in this checking really doesn't do much. I played with both embedded Jetty and using Tomcat as a container. The code here will make a generic Tomcat installation, but no real web pages.
    git-svn-id: https://avrs.svn.sourceforge.net/svnroot/avrs@26 08eb95d5-b051-4c95-8b77-12c5f7d83f87

--- src/net/ab0oo/aprs/parser/Utilities.java
+++ src/net/ab0oo/aprs/parser/Utilities.java
@@ -55,4 +55,31 @@ public class Utilities {
 	public static int ktsToMph(int knots) {
 		return (int)Math.round(knots * 1.15077945);
 	}
+	
+	public static double metersToMiles(double meters) {
+		return meters * 0.000621371192;
+	}
+	
+	public static double metersToKilometers(double meters) {
+		return meters * 1000.0;
+	}
+	
+	public static String degressToCardinal(double degrees) {
+		if ( degrees >= 11.25 && degrees < 33.75 ) return "NNE";
+		if ( degrees >= 33.75 && degrees < 56.25 ) return "NE";
+		if ( degrees >= 56.25 && degrees < 78.25 ) return "WNE";
+		if ( degrees >= 78.25 && degrees < 101.25 ) return "E";
+		if ( degrees >= 101.25 && degrees < 123.75 ) return "ESE";
+		if ( degrees >= 123.75 && degrees < 146.25 ) return "SE";
+		if ( degrees >= 146.25 && degrees < 168.75 ) return "SSE";
+		if ( degrees >= 168.75 && degrees < 191.25 ) return "S";
+		if ( degrees >= 191.25 && degrees < 213.75 ) return "SSW";
+		if ( degrees >= 213.75 && degrees < 236.25 ) return "SW";
+		if ( degrees >= 236.25 && degrees < 258.75 ) return "WSW";
+		if ( degrees >= 258.75 && degrees < 281.25 ) return "W";
+		if ( degrees >= 281.25 && degrees < 303.75 ) return "WNW";
+		if ( degrees >= 303.75 && degrees < 326.25 ) return "NW";
+		if ( degrees >= 326.25 && degrees < 348.75 ) return "NNW";
+		return "N";
+	}
 }
