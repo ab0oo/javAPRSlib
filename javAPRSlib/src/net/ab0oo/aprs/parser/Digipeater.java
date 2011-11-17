f6f223c84c0bd8957347aea7f58c37c209738628
ab0oo <ab0oo@arrl.net>
2011-11-16 22:09:44 -0500

    Initial GIT commit of javAPRSlib.  This code is STILL under the GPL, but it will be changed to the LGPL in the next commit.

---
+++ javAPRSlib/src/net/ab0oo/aprs/parser/Digipeater.java
@@ -0,0 +1,147 @@
+/*
+ * AVRS - http://avrs.sourceforge.net/
+ *
+ * Copyright (C) 2011 John Gorkos, AB0OO
+ *
+ * AVRS is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published
+ * by the Free Software Foundation; either version 2 of the License,
+ * or (at your option) any later version.
+ *
+ * AVRS is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with AVRS; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307
+ * USA
+ */
+package net.ab0oo.aprs.parser;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+
+/**
+ * 
+ * @author johng
+ * This class represents a single digipeater in a TNC2-format VIA string.
+ * 
+ */
+public class Digipeater implements Serializable {
+	private static final long serialVersionUID = 1L;
+	private String callsign;
+    private String ssid;
+    private boolean used;
+    
+    public Digipeater(String call) {
+        if ( call.indexOf("*") >= 0 ) {
+		setUsed(true);
+		call = call.substring(0, call.indexOf("*"));
+	}
+        if ( call.indexOf("-") > 0 ) {
+            ssid = call.substring(call.indexOf("-")+1, call.length());
+            this.callsign = call.substring(0,call.indexOf("-"));
+        } else {
+            this.callsign=call;
+            this.ssid="";
+        }
+    }
+    public Digipeater(byte[] data, int offset) {
+	    byte[] shifted = new byte[6];
+	    byte ssidbyte = data[offset + 6];
+	    for (int i = 0; i < 6; i++)
+		    shifted[i] = (byte)((data[offset + i]&0xff) >> 1);
+	    this.callsign = new String(shifted, 0, 6).trim();
+	    int ssidval = (ssidbyte & 0x1e) >> 1;
+	    if (ssidval != 0)
+		    this.ssid = "" + ssidval;
+	    else this.ssid = "";
+	    this.used = (ssidbyte & 0x80) == 0x80;
+    }
+
+    /** parse a comma-separated list of digipeaters
+     * @return the list of digipeaters as an array
+     */
+    public static ArrayList<Digipeater> parseList(String digiList, boolean includeFirst) {
+	String[] digiTemp = digiList.split(",");
+	ArrayList<Digipeater> digis = new ArrayList<Digipeater>();
+	int first = includeFirst ? 0 : 1;
+	// for now, '*' is set for all digis with used bit.
+	// however, only the last used digi should have a '*'
+	if ( digiTemp.length >= first ) {
+	    for (int i=first; i<digiTemp.length; i++) {
+		digis.add(new Digipeater(digiTemp[i]));
+	    }
+	}
+	return digis;
+    }
+
+    /**
+     * @return the callsign
+     */
+    public String getCallsign() {
+        return callsign;
+    }
+
+    /**
+     * @param callsign the callsign to set
+     */
+    public void setCallsign(String callsign) {
+        this.callsign = callsign;
+    }
+
+    /**
+     * @return the ssid
+     */
+    public String getSsid() {
+        return ssid;
+    }
+
+    /**
+     * @param ssid the ssid to set
+     */
+    public void setSsid(String ssid) {
+        this.ssid = ssid;
+    }
+
+    /**
+     * @return the used
+     */
+    public boolean isUsed() {
+        return used;
+    }
+
+    /**
+     * @param used the used to set
+     */
+    public void setUsed(boolean used) {
+        this.used = used;
+    }
+    
+    public String toString() {
+        return callsign+( ssid == "" ? "" : "-" )+ssid+ ( isUsed() ? "*":"");
+    }
+
+    public byte[] toAX25() throws IllegalArgumentException {
+        byte[] callbytes = callsign.getBytes();
+        byte[] ax25 = new byte[7];
+	// shift " " by one
+	java.util.Arrays.fill(ax25, (byte)0x40);
+	if (callbytes.length > 6)
+		throw new IllegalArgumentException("Callsign " + callsign + " is too long for AX.25!");
+        for (int i = 0; i < callbytes.length; i++) {
+		ax25[i] = (byte)(callbytes[i] << 1);
+	}
+	int ssidval = 0;
+	try {
+		ssidval = Integer.parseInt(ssid);
+	} catch (NumberFormatException e) {
+		// we ignore that for now.
+	}
+	// ssid byte: u11ssss0
+	ax25[6] = (byte) (0x60 | ((ssidval*2) & 0x1e) | (isUsed()?0x80:0));
+	return ax25;
+    }
+}
---
+++ javAPRSlib/src/net/ab0oo/aprs/parser/ObjectPacket.java
@@ -0,0 +1,86 @@
+package net.ab0oo.aprs.parser;
+
+import java.io.Serializable;
+
+public class ObjectPacket extends InformationField implements Serializable {
+	private static final long serialVersionUID = 1L;
+	private String objectName;
+	private boolean live = true;
+	private Position position;
+
+	/** parse an APRS object message
+	 * @return new ObjectPacket instance with the parsed data
+	 */
+	public ObjectPacket(byte[] msgBody) throws Exception {
+		this.objectName = new String(msgBody, 1, 9).trim();
+		this.live = (msgBody[10] == '*');
+		int cursor = 18;
+		if ( msgBody[cursor] > '0' && msgBody[cursor] < '9' ) {
+		    this.position = PositionParser.parseUncompressed(msgBody, cursor);
+		} else {
+		    this.position = PositionParser.parseCompressed(msgBody, cursor);
+		}
+		cursor += 19;
+		comment = new String(msgBody, cursor, msgBody.length - cursor, "UTF-8").trim();
+	}
+
+	public ObjectPacket( String objectName, boolean live, Position position, String comment) {
+		this.objectName = objectName;
+		this.live = live;
+		this.position = position;
+		this.comment = comment;
+	}
+
+	/**
+	 * @return the objectName
+	 */
+	public String getObjectName() {
+		return objectName;
+	}
+
+	/**
+	 * @param objectName the objectName to set
+	 */
+	public void setObjectName(String objectName) {
+		this.objectName = objectName;
+	}
+
+	/**
+	 * @return the live
+	 */
+	public boolean isLive() {
+		return live;
+	}
+
+	/**
+	 * @param live the live to set
+	 */
+	public void setLive(boolean live) {
+		this.live = live;
+	}
+
+	/**
+	 * @return the position
+	 */
+	public Position getPosition() {
+		return position;
+	}
+
+	/**
+	 * @param position the position to set
+	 */
+	public void setPosition(Position position) {
+		this.position = position;
+	}
+
+	/**
+	 * @param extension the extension to set
+	 */
+	public void setExtension(DataExtension extension) {
+		this.extension = extension;
+	}
+
+	public String toString() {
+		return ")"+this.objectName+( live ? "!":"_")+position.toString()+comment;
+	}
+}
